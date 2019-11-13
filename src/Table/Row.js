7cd82f97b29fe890d3f3a46f814977b75b889748
PrekoV <vikapredko1999@gmail.com>
2019-11-13 18:21:06 +0200

    wip on server

--- src/Table/Row.js
+++ src/Table/Row.js
@@ -1,28 +1,27 @@
-import React, { useState } from "react";
-import { Formik, Form, Field } from "formik";
+import React, { useState } from 'react';
+import { Formik, Form, Field } from 'formik';
 
-export default function Row({ item={}, onDelete, onEdit, onSave }) {
+export default function Row({ item = {}, onDelete, onEdit, onSave }) {
   const [isEdit, setIsEdit] = useState(!item.id);
 
   const toggleIsEdit = () => setIsEdit(!isEdit);
 
-  const onSubmit = (values) => {
+  const onSubmit = values => {
     toggleIsEdit();
     if (!isEdit) {
       return;
     }
-    onSave ? onSave(values) :
-    onEdit(item.id, values);
-  }
+    onSave ? onSave(values) : onEdit(item.id, values);
+  };
 
   return (
     <div>
       <Formik
         initialValues={{
-          name: item.name || "",
-          date: item.date || "",
-          position: item.position || "",
-          rate: item.rate || ""
+          name: item.name || '',
+          date: item.date || '',
+          position: item.position || '',
+          rate: item.rate || '',
         }}
         onSubmit={onSubmit}
       >
@@ -30,24 +29,12 @@ export default function Row({ item={}, onDelete, onEdit, onSave }) {
           return (
             <Form>
               <tr className="row">
-                <td className="name">
-                  {isEdit ? <Field name="name" type="text" /> : item.name}
-                </td>
-                <td className="date">
-                  {isEdit ? <Field name="date" type="text" /> : item.date}
-                </td>
-                <td className="position">
-                  {isEdit ? (
-                    <Field name="position" type="text" />
-                  ) : (
-                    item.position
-                  )}
-                </td>
-                <td className="rate">
-                  {isEdit ? <Field name="rate" type="text" /> : item.rate}
-                </td>
+                <td className="name">{isEdit ? <Field name="name" type="text" /> : item.name}</td>
+                <td className="date">{isEdit ? <Field name="date" type="text" /> : item.date}</td>
+                <td className="position">{isEdit ? <Field name="position" type="text" /> : item.position}</td>
+                <td className="rate">{isEdit ? <Field name="rate" type="text" /> : item.rate}</td>
                 <td className="action-buttons">
-                  <button type="submit">{isEdit ? "save" : "edit"}</button>
+                  <button type="submit">{isEdit ? 'save' : 'edit'}</button>
                   <button type="reset" onClick={() => onDelete(item.id)}>
                     delete
                   </button>
--- src/Table/Table.js
+++ src/Table/Table.js
@@ -1,58 +1,54 @@
-import React, { useState } from "react";
-import Row from "./Row";
+import React, { useState } from 'react';
+import Row from './Row';
 
 export default function Table() {
   const [items, setItems] = useState([
     {
       id: 2,
-      name: "name1",
-      date: "11/11/11",
+      name: 'name1',
+      date: '11/11/11',
       rate: 2000,
-      position: "developer"
+      position: 'developer',
     },
     {
       id: 28,
-      name: "name12",
-      date: "02/02/02",
+      name: 'name12',
+      date: '02/02/02',
       rate: 27000,
-      position: "team lead"
+      position: 'team lead',
     },
     {
       id: 3,
-      name: "name1424",
-      date: "10/6/2",
+      name: 'name1424',
+      date: '10/6/2',
       rate: 5000,
-      position: "developer php"
+      position: 'developer php',
     },
     {
       id: 25,
-      name: "name11212",
-      date: "11/10/01",
+      name: 'name11212',
+      date: '11/10/01',
       rate: 23000,
-      position: "developer Js"
-    }
+      position: 'developer Js',
+    },
   ]);
   const [isAddNew, setiIsAddNew] = useState(false);
 
   const toggleIsAddNew = () => setiIsAddNew(!isAddNew);
 
   const onDelete = id => {
-    console.log(
-      id,
-      items.filter(item => item.id !== id)
-    );
-
     setItems([...items.filter(item => item.id !== id)]);
   };
 
   const onEdit = (id, values) => {
-    console.log(values);
+    const currentValues = { ...values };
     const currentItems = [...items];
+    console.log(Object.prototype.hasOwnProperty.call(currentValues, 'name'));
 
     const currentItem = currentItems.find(item => item.id === id);
     if (currentItem) {
-      for (var key in currentItem) {
-        if (values.hasOwnProperty(key)) {
+      for (const key in currentItem) {
+        if (Object.prototype.hasOwnProperty.call(currentValues, key)) {
           currentItem[key] = values[key];
         }
       }
@@ -64,13 +60,11 @@ export default function Table() {
   };
 
   const onSave = values => {
-		const currentItems = [...items];
-		currentItems.push({ ...values, id: Math.random() });
-		setItems(currentItems);
-		toggleIsAddNew();
-	};
-
-  console.log(items);
+    const currentItems = [...items];
+    currentItems.push({ ...values, id: Math.random() });
+    setItems(currentItems);
+    toggleIsAddNew();
+  };
 
   return (
     <>
